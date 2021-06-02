8d62eb4c0371da32a2fe8e884e9712b5e8f3f56a
mktece <70434094+mktece@users.noreply.github.com>
2021-06-02 08:58:23 +0530

    Add files via upload

---
+++ lib/config/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorSys {
+  static Color primary = Color.fromRGBO(52, 43, 37, 1.0);
+  static Color gray = Color.fromRGBO(132, 132, 132, 1.0);
+  static Color secoundry = Color.fromRGBO(181, 27, 198, 1.0);
+  static Color secoundryLight = Color.fromRGBO(226, 141, 202, 1.0);
+}
---
+++ lib/config/constants.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static var stepOneTitle = "Soulmate App";
+  static var stepOneContent = "There are the apps of kikde is build to check your soulmate";
+  static var stepTwoTitle = "Kikde Tatkal";
+  static var stepTwoContent = "This app allow to book IRCTC train tatkal tickets, using profile forms and gives you guaranteed ticket in 20 Sec";
+  static var stepThreeTitle = "Kikde Spy";
+  static var stepThreeContent = "This is a hybrid software/service which allows you to monitor smartphone usage in real time and find all info about target device";
+}
